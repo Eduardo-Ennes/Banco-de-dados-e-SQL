be84c0e522dd615cc3aa95897398bf38b3983b67
Eduardo-Ennes <eduardosimoes9899@gmail.com>
2024-04-18 01:33:41 -0300

    add

--- Aula_10_Ifnull_View/Aula_01.sql
+++ Aula_10_Ifnull_View/Aula_01.sql
@@ -116,8 +116,10 @@ on f.idcliente = t.id_cliente;
 
 
 /* ----- VIEW ----- */ 
-/* Criando uma função view */
+/* É permitido fazer UPDATE na VIEW, irá alterar a tabela também*/
 
+
+/* Criando uma função view */
 create view V_RELATORIO as 
 select f.idcliente, f.nome, f.sexo, f.email, f.cpf, 
 e.rua, e.bairro, e.cidade, e.estado, 
@@ -134,5 +136,114 @@ drop view RELATORIO;
 
 
 select * from RELATORIO;
-
 /* Tabela ficou muito grande para ser colocada aqui. */ 
+
+
+select nome, sexo, bairro, numero
+from v_relatorio;
+
++------------+------+-----------------+----------+
+| nome       | sexo | bairro          | numero   |
++------------+------+-----------------+----------+
+| ANTONIO    | M    | BOM RETIRO      | 68976565 |
+| ANTONIO    | M    | BOM RETIRO      | 99656675 |
+| CARMEM     | F    | CENTRO          | 33567765 |
+| CARMEM     | F    | CENTRO          | 88668786 |
+| CARMEM     | F    | CENTRO          | 55689654 |
+| ADRIANA    | F    | CENTRO          | 88687979 |
+| JOICE      | F    | CENTRO          | 88965676 |
+| DANIELE    | F    | CENTRO          | 89966809 |
+| LORENA     | F    | LEBLON          | 88679978 |
+| EDUARDO    | M    | CENTRO          | 99655768 |
+| ANTONIO    | M    | JARDINS         | 89955665 |
+| ANTONIO    | M    | JARDINS         | 77455786 |
+| GIOVANA    | F    | CENTRO          | 89766554 |
+| ADRIANA    | F    | CENTRO          | 77755785 |
+| ANDRE      | M    | ESTACIO         | 44522578 |
+| Letiçia    | F    | Vila isabel     | 98653652 |
+| Maira      | F    | Leblon          | 98565326 |
+| Ronaldinho | M    | Ipanema         | 95822368 |
+| Celia      | F    | Barra da tijuca | 23651456 |
+| Fernando   | M    | Copacabana      | 85623659 |
+| Fernando   | M    | Copacabana      | 84715245 |
+| Barbara    | F    | Catete          | 23652369 |
+| Celia      | F    | Barra da tijuca | 45632145 |
++------------+------+-----------------+----------+
+
+
+/* ----- Continuando VIEW ----- */
+
+create table teste_view_jogadores(
+	ID int,
+	NOME varchar(30),
+	UF char(2)
+);
+
+
+insert into teste_view_jogadores values(1, 'Eden Hazard', 'RJ');
+insert into teste_view_jogadores values(2, 'Ganso', 'RJ');
+insert into teste_view_jogadores values(3, 'Nino', 'RJ');
+insert into teste_view_jogadores values(4, 'Gum', 'RJ');
+insert into teste_view_jogadores values(5, 'Washington','SP');
+
+
+select * from teste_view_jogadores;
+
++------+-------------+------+
+| ID   | NOME        | UF   |
++------+-------------+------+
+|    1 | Eden Hazard | RJ   |
+|    2 | Ganso       | RJ   |
+|    3 | Nino        | RJ   |
+|    4 | Gum         | RJ   |
+|    5 | Washington  | SP   |
++------+-------------+------+
+
+
+create view jogadores as 
+select id, nome, uf
+from teste_view_jogadores;
+
+
+select * from jogadores;
+
++------+-------------+------+
+| id   | nome        | uf   |
++------+-------------+------+
+|    1 | Eden Hazard | RJ   |
+|    2 | Ganso       | RJ   |
+|    3 | Nino        | RJ   |
+|    4 | Gum         | RJ   |
+|    5 | Washington  | SP   |
++------+-------------+------+
+
+
+update jogadores 
+set nome = 'Marcão'
+where id = 5;
+
+
+select * from jogadores;
+
++------+-------------+------+
+| id   | nome        | uf   |
++------+-------------+------+
+|    1 | Eden Hazard | RJ   |
+|    2 | Ganso       | RJ   |
+|    3 | Nino        | RJ   |
+|    4 | Gum         | RJ   |
+|    5 | Marcão      | SP   |
++------+-------------+------+
+
+
+select * from teste_view_jogadores;
+
++------+-------------+------+
+| ID   | NOME        | UF   |
++------+-------------+------+
+|    1 | Eden Hazard | RJ   |
+|    2 | Ganso       | RJ   |
+|    3 | Nino        | RJ   |
+|    4 | Gum         | RJ   |
+|    5 | Marcão      | SP   |
++------+-------------+------+
